f13e259f5de58d73fb3b2200a3372b0c7b8f7961
Carlos Mario Solano <krlosmario2805@hotmail.com>
2019-02-08 11:35:06 -0600

    Componente SeguroMedico Creado y mostrado n veces
    El componente de seguro medico esta creado (sin la parte gráfica) se agrega a su componente padre cada vez que se da click, pero aun no es borrado ni almacena los nombres de la aseguradora en Array.
    Actualmente estoy estudiando eventos de vue para poder borrar los componentes y almacenar su info

---
+++ Huli_Entrenamiento1-CarlosMario/src/ContenedorDeSeguroPlanMedico.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="ContenedorSeguros">
+    <SeguroPlanMedico v-for="i in datosSegurosPlanesMedicos"></SeguroPlanMedico>
+    <v-btn depressed large color="blue" @click="agregaSeguroPlanMedico()">{{
+      msjBtn
+    }}</v-btn>
+  </div>
+</template>
+
+<script>
+import SeguroPlanMedico from "./SeguroPlanMedico.vue";
+export default {
+  name: "ContenedorSeguros",
+  data() {
+    return {
+      msjBtn: "+ AGREGAR OTRO SEGURO O PLAN",
+      datosSegurosPlanesMedicos: []
+    };
+  },
+  components: {
+    SeguroPlanMedico
+  },
+  methods: {
+    agregaSeguroPlanMedico: function() {
+      this.datosSegurosPlanesMedicos.push("");
+    }
+  }
+};
+</script>
+
+<style lang="scss"></style>
---
+++ Huli_Entrenamiento1-CarlosMario/src/SeguroPlanMedico.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="SeguroPlanMedico">
+    <v-container>
+      <v-layout>
+        <v-flex>
+          <h3>{{ heart }}</h3>
+        </v-flex>
+        <v-flex>
+          <h3>{{ msjPrincipal }}</h3>
+        </v-flex>
+        <v-flex>
+          <v-btn flat small>X</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container>
+      <v-layout>
+        <v-flex>
+          <v-autocomplete
+            id="in_Aseguradora"
+            placeholder="Aseguradora/Institucion"
+          ></v-autocomplete>
+        </v-flex>
+        <v-flex>
+          <v-text-field
+            id="in_Numero"
+            placeholder="Poliza/numero de afiliado"
+          ></v-text-field>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SeguroPlanMedico",
+  data() {
+    return {
+      msjPrincipal: "Seguro o plan medico",
+      heart: "<3"
+    };
+  },
+  components: {}
+};
+</script>
+
+<style lang="scss">
+h3 {
+  color: black;
+}
+</style>
